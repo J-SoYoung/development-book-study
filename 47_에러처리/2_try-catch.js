ac7a5e3ec8b117561dd32c46bc272721b82d52ec
soyoung <thgml1444@naver.com>
2022-12-07 13:51:13 +0900

    47_ 에러처리, 혼공JS스터디, deep요약

--- 47_에러처리/2_try-catch.js
+++ 47_에러처리/2_try-catch.js
@@ -1,15 +1,15 @@
 
 
-// 에러처리의 필요성
-// 에러처리를 하지 않으면 프로그램은 에러를 내고 강제종료된다.
+// // 에러처리의 필요성
+// // 에러처리를 하지 않으면 프로그램은 에러를 내고 강제종료된다.
 
-// console.log('start')
-// foo()
-// console.log('end')
+// // console.log('start')
+// // foo()
+// // console.log('end')
 
 
 
-// try-catch-finally문
+// // try-catch-finally문
 // console.log('start됨')
 // try {
 //   foo()
@@ -20,10 +20,11 @@
 //   // console.log('에러발생',error)
 //   // try 코드 블록에서 에러가 발생하면 이 코드 블록의 코드가 실행된다
 //   // try 코드문에서 error객체를 생성하기 때문에 매개변수로 error코드를 확인해볼 수 있다
-// } finally{
-//   console.log('finally문: 에러발생 여부와 상관없이 출력')
-//   // 에러 발생과 상관없이 무조건 실행되는 코드
-// }
+// } 
+// // finally{
+// //   console.log('finally문: 에러발생 여부와 상관없이 출력')
+// //   // 에러 발생과 상관없이 무조건 실행되는 코드
+// // }
 // console.log('end됨. try catch문을 활용하면 프로그램이 강제종료 되지 않는다')
 
 
@@ -31,14 +32,14 @@
 
 // Error 객체 종류
 // 자바스크립트에서 error객체를 생성한다. 
-// try {
-//   foo()
-//   // null.foo 
-//   // new Array(-1)
-//   // 실행할 코드
-// } catch(error){
-//   console.log('catch문:', error)
-// } finally{
-//   console.log('finally문')
-// }
-// console.log('end됨')
+try {
+  // foo()
+  // null.foo 
+  new Array(-1)
+  // 실행할 코드
+} catch(error){
+  console.log('catch문:', error)
+} finally{
+  console.log('finally문')
+}
+console.log('end됨')
--- 47_에러처리/3_finally.js
+++ 47_에러처리/3_finally.js
@@ -2,17 +2,17 @@
 
 // finally키워드 사용 안했을 때
 // return구문을 만나면 try-catch 문을 빠져나간다. 마지막 C위치를 출력하지 않고 나가버림
-function test() {
-  try {
-    console.log('A 위치입니다.')
-    throw "예외 강제 발생"
-  } catch (exception) {
-    console.log('B 위치입니다.')
-    return
-  } 
-  console.log('C 위치입니다.')
-}
-test()
+// function test() {
+//   try {
+//     console.log('A 위치입니다.')
+//     // throw "예외 강제 발생"
+//   } catch (exception) {
+//     console.log('B 위치입니다.')
+//     return
+//   } 
+//   console.log('C 위치입니다.')
+// }
+// test()
 
 
 // finally키워드 사용 했을 때
