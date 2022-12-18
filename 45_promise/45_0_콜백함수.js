10bcec65cd032e5a5a9cf994df97971fa0e282f2
soyoung <thgml1444@naver.com>
2022-12-19 08:49:00 +0900

    promise-update

---
+++ 45_promise/45_0_콜백함수.js
@@ -0,0 +1,49 @@
+// // 외부에서 전달받은 f를 n만큼 반복 호출한다.
+// -------------------------------- deep 
+// function repeat(n, f) {
+  //   for (var i = 0; i < n; i++) {
+//     f(i); // i를 전달하면서 f를 호출
+//   }
+// }
+// var logAll = function (i) {
+  //   console.log(i);
+  // };
+  
+  // // 반복 호출할 함수를 인수로 전달한다.
+  // repeat(5, logAll); // 0 1 2 3 4
+  
+  
+// -------------------------------- deep 
+// var logOdds = function (i) {
+//   if (i % 2) console.log(i);
+// };
+
+// // 반복 호출할 함수를 인수로 전달한다.
+// repeat(5, logOdds); // 1 3
+
+
+//---------------------------callback 흐름
+// const val = function(a){
+//   return console.log('thdud')
+//   } 
+// function fn(arg){
+//   console.log('1번')
+//   arg()
+//   console.log('2번')
+// }
+// fn(val)
+// console.log('3번') // 1번 thdud 2번 3번
+
+
+// -------------- filter함수를 콜백으로 풀어서 사용해봄
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+function callback(ele){
+  if(ele.length > 6){
+    return true
+  }else {
+    return false
+  }
+}
+const newords = words.filter(callback)
+console.log(newords)
---
+++ 45_promise/45_promise.js
@@ -0,0 +1,69 @@
+const request = {
+  get(url) {
+    return fetch(url);
+  },
+  post(url, payload) {
+    return fetch(url, {
+      method: 'POST',
+      headers: { 'content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    });
+  },
+  patch(url, payload) {
+    return fetch(url, {
+      method: 'PATCH',
+      headers: { 'content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    });
+  },
+  delete(url) {
+    return fetch(url, { method: 'DELETE' });
+  }
+};
+
+
+// get요청
+request.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {
+    if (!response.ok) throw new Error(response.statusText);
+    return response.json();
+  })
+  .then(todos => console.log(todos))
+  .catch(err => console.error(err));
+// {userId: 1, id: 1, title: "delectus aut autem", completed: false}
+
+// post요청
+request.post('https://jsonplaceholder.typicode.com/todos', {
+  userId: 1,
+  title: 'JavaScript',
+  completed: false
+}).then(response => {
+    if (!response.ok) throw new Error(response.statusText);
+    return response.json();
+  })
+  .then(todos => console.log(todos))
+  .catch(err => console.error(err));
+// {userId: 1, title: "JavaScript", completed: false, id: 201}
+
+
+// patch요청
+request.patch('https://jsonplaceholder.typicode.com/todos/1', {
+  completed: true
+}).then(response => {
+    if (!response.ok) throw new Error(response.statusText);
+    return response.json();
+  })
+  .then(todos => console.log(todos))
+  .catch(err => console.error(err));
+// {userId: 1, id: 1, title: "delectus aut autem", completed: true}
+
+
+// delete 요청
+request.delete('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {
+    if (!response.ok) throw new Error(response.statusText);
+    return response.json();
+  })
+  .then(todos => console.log(todos))
+  .catch(err => console.error(err));
+// {}
