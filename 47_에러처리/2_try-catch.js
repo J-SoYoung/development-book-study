e5e52306b8567d26398a737bf45108a2b885e5f9
thdud <thgml1444@naver.com>
2023-08-16 20:16:30 +0900

    [ Error ] 다양한 상황의 에러 처리를 해 놓아야 사용자, 개발자가 상황에 빠르게 대처할 수 있다

--- 47_에러처리/2_try-catch.js
+++ 47_에러처리/2_try-catch.js
@@ -1,45 +1,22 @@
+// 에러처리의 필요성
+// 에러처리를 하지 않으면 프로그램은 에러를 내고 강제종료된다.
 
+//1_ 에러발생 프로그램 강제 종료됨
+// console.log("start");
+// foo();
+// console.log("end");
 
-// // 에러처리의 필요성
-// // 에러처리를 하지 않으면 프로그램은 에러를 내고 강제종료된다.
-
-// // console.log('start')
-// // foo()
-// // console.log('end')
-
-
-
-// // try-catch-finally문
-// console.log('start됨')
-// try {
-//   foo()
-//   // 실행할 코드
-// } catch(error){
-//   console.log('catch문: error객체 출력가능')
-//   console.log(error)
-//   // console.log('에러발생',error)
-//   // try 코드 블록에서 에러가 발생하면 이 코드 블록의 코드가 실행된다
-//   // try 코드문에서 error객체를 생성하기 때문에 매개변수로 error코드를 확인해볼 수 있다
-// } 
-// // finally{
-// //   console.log('finally문: 에러발생 여부와 상관없이 출력')
-// //   // 에러 발생과 상관없이 무조건 실행되는 코드
-// // }
-// console.log('end됨. try catch문을 활용하면 프로그램이 강제종료 되지 않는다')
-
-
-
-
-// Error 객체 종류
-// 자바스크립트에서 error객체를 생성한다. 
+// 2_try-catch-finally문
+console.log("start됨");
 try {
-  // foo()
-  // null.foo 
-  new Array(-1)
-  // 실행할 코드
-} catch(error){
-  console.log('catch문:', error)
-} finally{
-  console.log('finally문')
+  foo();
+} catch (error) {
+  // try 코드 블록에서 에러가 발생하면 이 코드 블록의 코드가 실행된다
+  // try 코드문에서 error객체를 생성하기 때문에 매개변수로 error코드를 확인해볼 수 있다
+  console.log("catch문: error객체 출력가능", error);
 }
-console.log('end됨')
+finally{
+  // 에러 발생과 상관없이 무조건 실행되는 코드
+  console.log('finally문: 에러발생 여부와 상관없이 출력')
+}
+console.log("end됨. try catch문을 활용하면 프로그램이 강제종료 되지 않는다");
